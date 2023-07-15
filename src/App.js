645b4a6d14b898a71e70b65f5a430c2d4119b13d
Zachary Kuchar <zacharyk323@live.com>
2023-07-15 10:06:25 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import responses from "./models/responses";
+import { useState, useEffect } from "react";
+import Display from "./components/Display";
 
 function App() {
+  const [responseList, setResponseList] = useState(responses);
+  const [selection, setSelection] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Magic eight ball</h1>
+      <Display item={responseList[0]} />
     </div>
   );
 }
---
+++ src/components/Display.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Display(props) {
+  return (
+    <div className='display'>
+      {props.item}
+    </div>
+  )
+}
+
+export default Display
